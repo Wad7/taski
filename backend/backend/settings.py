ed3dec894a4519573f295a2f3de511b88aba533b
Wadim <yc-user@epdkn5naovbdlgkfv65t.auto.internal>
2023-06-10 20:35:12 +0000

    Add workflow with PEP8 linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY= 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
 DEBUG = True
 
@@ -73,7 +73,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
      'NAME':
-      'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+         'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
      'NAME':
@@ -83,7 +83,8 @@ AUTH_PASSWORD_VALIDATORS = [
      'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+     'NAME':
+      'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
