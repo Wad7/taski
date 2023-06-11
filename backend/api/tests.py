4fa86e7805152e555a21fed82b9c242661219016
Wadim <yc-user@epdkn5naovbdlgkfv65t.auto.internal>
2023-06-11 21:06:36 +0000

    Add workflow with PEP8 linting

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -3,10 +3,10 @@ from http import HTTPStatus
 from api import models
 from django.test import Client, TestCase
 
+
 class TaskiAPITestCase(TestCase):
     def setUp(self):
         self.guest_client = Client()
-
 
     def test_list_exists(self):
         """Проверка доступности списка задач."""
